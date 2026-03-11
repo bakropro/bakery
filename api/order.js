eda7089feb63cd010e6b80276159838639b24926
bakropro <leksobak@gmail.com>
2026-03-11 22:04:54 +0300

    new

--- api/order.js
+++ api/order.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
   try {
 
     for (const chatId of ADMIN_CHAT_IDS) {
-      await fetch(url, {
+      await fetch("/api/order", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
